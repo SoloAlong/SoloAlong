7a295be884e0051585134e1e67a534dcff21145f
Yueyue07 <xww.qyy@gmail.com>
2016-02-01 12:16:55 -0800

    add server for test purpose

---
+++ test/test_server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = module.exports = exports = express();
+const mongoose = require('mongoose');
+const authRouter = require(__dirname + '/../routes/auth_router.js');
+mongoose.connect('mongodb://localhost/test_app_dev');
+
+app.use(authRouter);
+
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('server up'));
