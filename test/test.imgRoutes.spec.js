c31ebb42870b08a51dc6bf457baef3ed3c60609f
jamesvermillion <jamesvermillion@yahoo.com>
2016-02-01 12:42:55 -0800

    added images to chord library and made tests to make sure paths are valid

---
+++ test/test.imgRoutes.spec.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const expect = require('chai').expect;
+const chords = require(__dirname + '/../lib/logic/chords');
+const fs = require('fs');
+
+describe('Check to see if all paths to images work from chord object', () => {
+  var ch = Object.keys(chords);
+  for (let i = 0; i < ch.length; i++) {
+    it(ch[i] + ' should have a real filepath to its images', () => {
+      let key = ch[i];
+      let path = chords[key].image;
+      let f = fs.readFileSync(path);
+      expect(f instanceof Buffer).to.eql(true);
+    });
+  }
+});
