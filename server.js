5c0f2e959a3a2125a197de972b14efba3461dd93
Cameron Forras <cameronforras@hotmail.com>
2016-02-01 11:03:44 -0800

    added html page and modified server.js

--- server.js
+++ server.js
@@ -1,7 +1,39 @@
 const express = require('express');
 const app = module.exports = exports = express();
+const jsonParser = require('body-parser').json();
 const mongoose = require('mongoose');
+const fs = require('fs');
+
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/soloDev');
+
+var Message = mongoose.model('Message', new mongoose.Schema({
+  msg: String
+}));
+
+
+app.post('/api/message', jsonParser, (req, res) => {
+  var msg = new Message(req.body);
+  msg.save((err, data) => {
+    if (err) return res.status(500).json({msg: 'server err'});
+
+    res.json(msg);
+  });
+});
+
+app.get('/', (req, res) => {
+  var index = fs.createReadStream(__dirname + '/index.html');
+  index.pipe(res);
+});
+
+app.get('/index', (req, res) => {
+  var index = fs.createReadStream(__dirname + '/index.html');
+  index.pipe(res);
+});
+
+app.get('/home', (req, res) => {
+  var index = fs.createReadStream(__dirname + '/index.html');
+  index.pipe(res);
+});
 
 const s_router = require(__dirname + '/routes/router');
 const authRouter = require(__dirname + '/routes/auth_router');
