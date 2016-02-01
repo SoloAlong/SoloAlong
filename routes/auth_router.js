2003f1bca06801fa4c4fed3d0ddaf8a5b0ce7c3d
Yueyue07 <xww.qyy@gmail.com>
2016-02-01 11:10:04 -0800

    add check unique user when sign in and email validation check

--- routes/auth_router.js
+++ routes/auth_router.js
@@ -3,38 +3,39 @@ const User = require(__dirname + '/../models/user');
 const jsonParser = require('body-parser').json();
 const handleDBError = require(__dirname + '/../lib/handleDBError');
 const basicHTTP = require(__dirname + '/../lib/basic_http');
+const saveUserDB = require(__dirname + '/../lib/save_new_user');
+const emailValidation = require(__dirname + '/../lib/email_validation.js');
 
 var authRouter = module.exports = exports = express.Router();
 
 authRouter.post('/signup', jsonParser, (req, res) => {
 
-  var newUser = new User();
-  if(!((req.body.email || '').length && (req.body.password || '').length > 3)){
-    return res.status(401).json({msg: 'invalid username or password'});
-  }
+  if (!(req.body.email || '').length && emailValidation(req.body.email)) return res.status(200).json( { msg: 'Please enter a email' } );
 
-  newUser.username = req.body.username || req.body.email;
-  newUser.authentication.email = req.body.email;
-  newUser.hashPassword(req.body.password);
-  newUser.save((err, data) => {
-    if(err) return handleDBError(err, res);
+  if (!(req.body.username || '').length) return res.status(200).json( { msg: 'Please enter a user name' } );
 
-    res.status(200).json({token: data.generateToken()});
+  if (!(req.body.password || '').length > 7) return res.status(200).json( { msg: 'Please enter password' } );
+
+
+  User.find({ $or: [ { 'username': req.body.username }, { 'email': req.body.email } ] }, (err, data) => {
+    if (err) return handleDBError(err, res);
+    if (data.length) return res.status(200).json( { msg: 'user already exist; plese sign in this site' } );
+    saveUserDB(req, res);
   });
 });
 
 authRouter.post('/signin', basicHTTP, (req, res) => {
 
-  User.findOne({'authentication.email': req.basicHTTP.email}, (err, user) => {
-    if(err) {
+  User.findOne( { 'authentication.email': req.basicHTTP.email }, (err, user) => {
+    if (err) {
       console.log(err);
-      return res.status(401).json({msg: 'invalid username or password'});
+      return res.status(401).json( { msg: 'invalid username or password' } );
     }
 
-    if(!user) return res.status(401).json({msg: 'invalid username or password'});
+    if (!user) return res.status(401).json( { msg: 'invalid username or password' } );
 
-    if(!user.comparePassword(req.basicHTTP.password)) return res.status(401).json({msg: 'invalid username or password'});
+    if (!user.comparePassword(req.basicHTTP.password)) return res.status(401).json( { msg: 'invalid username or password' } );
 
-    res.json({token: user.generateToken()}); //no failures, so send back a token
+    res.json( { token: user.generateToken() } );
   });
 });
