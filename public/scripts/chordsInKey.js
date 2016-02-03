da9d913fdfa078ed80d39b6b7e1c68e001c595d1
jamesvermillion <jamesvermillion@yahoo.com>
2016-02-03 12:09:43 -0800

    got chords to come up for every key

--- public/scripts/chordsInKey.js
+++ public/scripts/chordsInKey.js
@@ -1,23 +1,59 @@
 $(function() {
-  $.get('/../template.html', function(data) {
-    var template = Handlebars.compile(data);
-    $.ajax({
-      contentType: 'application/json',
-      headers: {
-        token: $.cookie('token')
-      },
-      dataType: 'json',
-      success: function(theta) {
-        console.log(theta);
-        for (var i = 0; i < theta.length; i += 1) {
-          var html = template(theta[i]);
+  var key;
+  var orientation;
+  function dropChange(){
+    key = $('#letter').val(); 
+    orientation = $('#orientation').val();
+    console.log(key);
+    //get 7 chords
+    $.get('/../template_chordsInKey.html', function(data) {
+      var template = Handlebars.compile(data);
+
+      $.ajax({
+        type: 'GET',
+        url: '/chordsInKeyz',
+        contentType: 'application/json',
+        headers: {
+          token: $.cookie('token'),
+          key: key,
+          orientation: orientation
+        },
+        processData: false,
+        //dataType: 'string',
+        success: function(theta) {
+          console.log(theta);
+          var html = template(theta);
+          $('#chords').empty();
           $('#chords').append(html);
-        }
-      },
-      error: function() { console.log('Device control failed'); },
-      processData: false,
-      type: 'GET',
-      url: '/profile'
+        },
+        error: function() { console.log('Device control failed'); }
+        
+      });
     });
-  });
+
+  }
+  $('#letter').change(dropChange);
+  $('#orientation').change(dropChange);
+
+  // $.get('/../template_chordsInKey.html', function(data) {
+  //   var template = Handlebars.compile(data);
+  //   $.ajax({
+  //     contentType: 'application/json',
+  //     headers: {
+  //       token: $.cookie('token')
+  //     },
+  //     dataType: 'json',
+  //     success: function(theta) {
+  //       console.log(theta);
+  //       for (var i = 0; i < theta.length; i += 1) {
+  //         var html = template(theta[i]);
+  //         $('#chords').append(html);
+  //       }
+  //     },
+  //     error: function() { console.log('Device control failed'); },
+  //     processData: false,
+  //     type: 'GET',
+  //     url: '/profile'
+  //   });
+  // });
 });
