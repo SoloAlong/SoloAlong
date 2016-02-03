40a449e06f38597afceaf575fcd3535f9a249c92
Yueyue07 <xww.qyy@gmail.com>
2016-02-02 20:01:00 -0800

    add siginin test for auth_router

--- test/auth_router_test.js
+++ test/auth_router_test.js
@@ -15,7 +15,11 @@ describe('User Authentication: ', () => {
   });
   describe('User signup test: ', () => {
     it('should be able to remind user to enter a email if it does not exist', (done) => {
-      var invalidUser = { email: '', username: 'newuser1', password: '12345678' };
+      var invalidUser = {
+        email: '',
+        username: 'newuser1',
+        password: '12345678'
+      };
       chai.request(origin)
         .post('/signup')
         .send(invalidUser)
@@ -27,7 +31,11 @@ describe('User Authentication: ', () => {
         });
     });
     it('should be able to remind user to re-enter a email if it is invalid', (done) => {
-      var invalidUser = { email: 'user.', username: 'newuser1', password: '12345678' };
+      var invalidUser = {
+        email: 'user.',
+        username: 'newuser1',
+        password: '12345678'
+      };
       chai.request(origin)
         .post('/signup')
         .send(invalidUser)
@@ -39,7 +47,11 @@ describe('User Authentication: ', () => {
         });
     });
     it('should be able to remind user to enter a username', (done) => {
-      var invalidUser = { email: 'newuser1@gmail.com', username: '', password: '12345678' };
+      var invalidUser = {
+        email: 'newuser1@gmail.com',
+        username: '',
+        password: '12345678'
+      };
       chai.request(origin)
         .post('/signup')
         .send(invalidUser)
@@ -52,7 +64,11 @@ describe('User Authentication: ', () => {
     });
 
     it('should be able to remind user to enter a password if its length is less than 7 characters', (done) => {
-      var invalidUser = { email: 'newuser1@gmail.com', username: 'newuser1', password: '123456' };
+      var invalidUser = {
+        email: 'newuser1@gmail.com',
+        username: 'newuser1',
+        password: '123456'
+      };
       chai.request(origin)
         .post('/signup')
         .send(invalidUser)
@@ -65,7 +81,12 @@ describe('User Authentication: ', () => {
     });
 
     it('it should be able to create a new  user', (done) => {
-      var newUser = { email: 'newuser@gmail.com', username: 'newuser', password: '12345678', confirmpassword: '12345678' };
+      var newUser = {
+        email: 'newuser@gmail.com',
+        username: 'newuser',
+        password: '12345678',
+        comfirmpassword: '12345678'
+      };
       chai.request(origin)
         .post('/signup')
         .send(newUser)
@@ -79,7 +100,12 @@ describe('User Authentication: ', () => {
     });
 
     it('should be able to check whether the user already exist for signup', (done) => {
-      var sameUser = { email: 'newuser@gmail.com', username: 'newuser', password: '12345678', confirmpassword: '12345678' };
+      var sameUser = {
+        email: 'newuser@gmail.com',
+        username: 'newuser',
+        password: '12345678',
+        comfirmpassword: '12345678'
+      };
       chai.request(origin)
         .post('/signup')
         .send(sameUser)
@@ -92,4 +118,56 @@ describe('User Authentication: ', () => {
     });
   });
 
+  describe('user signin test:', () => {
+    var signinuser = {
+      email: 'signinuser@gmail.com',
+      username: 'signinuser',
+      password: '12345678',
+      comfirmpassword: '12345678'
+    };
+    before((done) => {
+      chai.request('localhost:4000')
+        .post('/signup')
+        .send(signinuser)
+        .end((err, res) => {
+          expect(err).to.eql(null);
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+
+    it('should be able to signin if user valid', (done) => {
+      chai.request(origin)
+        .get('/signin')
+        .auth(signinuser.email, signinuser.password)
+        .end((err, res) => {
+          expect(err).to.eql(null);
+          expect(res.body.msg).to.eql('Success in signin');
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+    it('should be able to avoid user to login if user doesn not exist', (done) => {
+      chai.request(origin)
+        .get('/signin')
+        .auth('nouser@gmail.com', '12345678')
+        .end((err, res) => {
+          expect(err).to.eql(null);
+          expect(res).to.have.status(401);
+          expect(res.body.msg).to.eql('no user exists');
+          done();
+        });
+    });
+    it('should be able to avoid user to login if password is incorrect', (done) => {
+      chai.request(origin)
+        .get('/signin')
+        .auth('signinuser@gmail.com', '12345679')
+        .end((err, res) => {
+          expect(err).to.eql(null);
+          expect(res).to.have.status(401);
+          expect(res.body.msg).to.eql('incorrect password');
+          done();
+        });
+    });
+  });
 });
