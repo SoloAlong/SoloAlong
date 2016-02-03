93233187570ae6c5e8b1a449aa90bd93dc40d902
jamesvermillion <jamesvermillion@yahoo.com>
2016-02-03 08:53:54 -0800

    hold onto scratches with chordsInKey

---
+++ public/scripts/chordsInKey.js
@@ -0,0 +1,23 @@
+$(function() {
+  $.get('/../template.html', function(data) {
+    var template = Handlebars.compile(data);
+    $.ajax({
+      contentType: 'application/json',
+      headers: {
+        token: $.cookie('token')
+      },
+      dataType: 'json',
+      success: function(theta) {
+        console.log(theta);
+        for (var i = 0; i < theta.length; i += 1) {
+          var html = template(theta[i]);
+          $('#chords').append(html);
+        }
+      },
+      error: function() { console.log('Device control failed'); },
+      processData: false,
+      type: 'GET',
+      url: '/profile'
+    });
+  });
+});
