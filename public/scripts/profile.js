027f0d5ff79998940e9b6772352a12e3976732f0
Yueyue07 <xww.qyy@gmail.com>
2016-02-04 11:24:07 -0800

    change template.html to template_chord.html; change user_template.html to template_userinfo.html

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,5 +1,5 @@
 $(() => {
-  $.get('/../user_template.html', (data) => {
+  $.get('/../template_userinfo.html', (data) => {
     var template = Handlebars.compile(data);
     $.ajax({
       contentType: 'application/json',
@@ -20,7 +20,7 @@ $(() => {
       error: function(data) { console.log(data); }
     });
   });
-  $.get('/../template.html', (data) => {
+  $.get('/../template_chords.html', (data) => {
     var template = Handlebars.compile(data);
     $.ajax({
       contentType: 'application/json',
@@ -48,5 +48,5 @@ $(() => {
       window.location.href = '/chordsInKey';
       return false;
   });
- 
+
 });
